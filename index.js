6d4820bf90bef3e8ccb7bb36773d9e59c8f6cb66
FeatherVeil <slayerplayerutg@gmail.com>
2026-07-27 20:07:07 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+const mineflayer = require('mineflayer')
+
+mineflayer.createBot({
+  host: 'eternalzero.mcsh.io',
+  port: 25565,
+  username: 'AFKBot',
+  auth: 'offline',
+  version: '1.21.6'
+})
